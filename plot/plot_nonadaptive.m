46921f7e526f0092abf58fb035a9a012a339aa01
scchow <schow@hmc.edu>
2017-11-08 18:27:20 -0800

    Standardized plotting

--- plot/plot_nonadaptive.m
+++ plot/plot_nonadaptive.m
@@ -5,18 +5,18 @@ setPlot();
 epochs = 3000;
 nights = 10;
 capacity = 10;
-numTrials = 3;
+numTrials = 20;
 numAgents = 100;
 
 date = '2017-11-03_10-02-19';
 
-% numDisabled = {'0', '20', '50', '60', '70', '90'};
-numDisabled = {'0'};
+numDisabled = {'0', '20', '50', '60', '70', '90'};
+% numDisabled = {'0'};
 
 % paths = arrayfun(@(x) strcat('../build/Results/', date, '/MultiNightBarQ/non-adaptive', ...
 %     "/", num2str(numAgents),'_agents/',x,'_disabled'),numDisabled);
 
-paths = arrayfun(@(x) strcat('results_11-8/final/MultiNightBarQ/non-adaptive', ...
+paths = arrayfun(@(x) strcat('results_11-8/final_discount0/MultiNightBarQ/non-adaptive', ...
     "/", num2str(numAgents),'_agents/',x,'_disabled'),numDisabled);
 
 dataDict = containers.Map();
@@ -49,32 +49,56 @@ end
 
 
 
-markers = ['o'; 'v'; 'x'; 's'; 'p'; '^'; '<'; '>'];
-linestyles = {'-'; '--'; '-.'; ':'};
+markers = ['x'; 'o'; 'v'; 's'; '^'; 'd'; 'p'];
+linestyles = {'-.'; '-'; '--'};
+c = get(gca, 'colororder');
+% '-' = baseline
+% 'o' for original
+set(gcf, 'Position', [1000, 800, 560, 420])
+set(gca, 'FontName', 'Times New Roman');
+
 
 increment = 200;
 maxEpoch = 3000;
 dict_keys = numDisabled;
 
+plotHandles = zeros(length(dict_keys),1);
+errHandles = zeros(length(dict_keys),1);
+sampleHandles = zeros(length(dict_keys),1);
+
 for i = 1:length(dict_keys)
     key = dict_keys{i};
     value = dataDict(key);
+    epochs = value(:,1);
+    means = value(:,2);
+    stderr = value(:,3);
+    
     x_axis = value(1:increment:maxEpoch,1);
     y_axis = value(1:increment:maxEpoch,2);
     errors = value(1:increment:maxEpoch,3);
-    e = errorbar(x_axis, y_axis, errors, ...
-        'Marker', markers(mod(i,length(markers))), ...
-        'Linestyle', linestyles{1+mod(i, length(linestyles))} ...
-        );
+%     e = plot(x_axis, y_axis,... errors, ...
+%         'Marker', markers(mod(i,length(markers))), ...
+%         'Linestyle', linestyles{1+mod(i, length(linestyles))} ...
+%         );
+    
+    % Plot line
+    ls = linestyles{1 + mod(i, length(linestyles))};
+    plotHandles(i) = plot(epochs, means, 'LineStyle', ls, 'LineWidth', 2, 'Color', c(i,:));
     hold on
+    errHandles(i) = errorbar(x_axis, y_axis, errors, 'LineStyle', 'None', 'Marker', markers(mod(i,length(markers))), 'Color', c(i,:));
+    sampleHandles(i) = errorbar(x_axis(1), y_axis(1), errors(1), 'LineStyle', ls, 'Color', c(i,:),'Marker', markers(mod(i,length(markers))));
+    
 
 end
 
-title(strcat('Performance vs Number of Epochs for ', num2str(nights), ...
-    ' Nights of ', num2str(capacity), ' Capacity with ', num2str(numAgents), ' Adaptive ', 'Agents'));
-xlabel('Number of Epochs');
-ylabel('Performance (max 100)');
-legend(numDisabled);
+% title(strcat('Performance vs Number of Epochs for ', num2str(nights), ...
+%     ' Nights of ', num2str(capacity), ' Capacity with ', num2str(numAgents), ' Adaptive ', 'Agents'));
+
+xlabel('Number of Epochs', 'FontSize', 14, 'Interpreter', 'latex');
+ylabel('Performance (max 100)', 'FontSize', 14, 'Interpreter', 'latex');
+legend(sampleHandles,numDisabled, 'Location', 'SouthEast', 'Interpreter', 'latex');
+
+export_fig(gcf, 'bar_nonadaptive.pdf', '-trans');
 
 end
 
